18cbb3de9d8eb899fef662f821cf864f841621e2
Illgner Alain <illgneralain@gmail.com>
2021-04-11 01:24:30 -0400

    created an object store

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -1,3 +1,9 @@
 let db; 
 
 const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = function(e) {
+    const db = request.result;
+    db.createObjectStore('pending', { autoIncrement: true });
+    store.clear();
+  };
