a7b4f765003d1e5344efba6c8977978dab915a27
Illgner Alain <illgneralain@gmail.com>
2021-04-11 01:27:12 -0400

    if app is online check database

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -3,7 +3,16 @@ let db;
 const request = indexedDB.open('budget', 1);
 
 request.onupgradeneeded = function(e) {
-    const db = request.result;
+    const db = e.target.result;
     db.createObjectStore('pending', { autoIncrement: true });
     store.clear();
   };
+
+  request.onsuccess = function(e) {
+    db = e.target.result;
+    // tx = db.transaction(storeName, "readwrite");
+    // store = tx.objectStore(storeName);
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
