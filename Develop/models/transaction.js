913dc2775178fb0ddaf19b381891a5cb41d9f6a6
Illgner Alain <illgneralain@gmail.com>
2021-04-10 23:50:22 -0400

    adding some offline data

--- Develop/models/transaction.js
+++ Develop/models/transaction.js
@@ -20,6 +20,22 @@ const transactionSchema = new Schema(
   }
 );
 
+const transactionSchema = new Schema ({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for transaction"
+  },
+  value: {
+    type: Number,
+    required: "Enter an amount"
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
 const Transaction = mongoose.model("Transaction", transactionSchema);
 
 module.exports = Transaction;
